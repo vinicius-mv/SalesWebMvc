3edb519661b4d940b3ce3b4fa20597d5d73ed690
Vinícius Martins <viniciusmv91@gmail.com>
2021-04-01 22:36:19 -0300

    SellersController

---
+++ SalesWebMvc/Controllers/SellersController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace SalesWebMvc.Controllers
+{
+    public class SellersController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
